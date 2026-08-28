17476bea5050ffbf3428784de78c83c8ac58c2a5
Fizza Hussain <fizzashah0300@gmail.com>
2026-08-29 00:48:00 +0500

    feat: integrate with Windows desktop
    Add fullscreen desktop behavior, Windows startup launch, safe quit shortcut, and NSIS packaging.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen, globalShortcut } = require('electron');
 const path = require('path');
 
 function createWindow() {
+  const display = screen.getPrimaryDisplay();
+  const { width, height } = display.bounds;
+
   const win = new BrowserWindow({
-    width: 1440,
-    height: 900,
+    x: 0,
+    y: 0,
+    width,
+    height,
     frame: false,
-    resizable: true,
+    resizable: false,
+    movable: false,
+    minimizable: false,
+    maximizable: false,
+    skipTaskbar: true,
+    alwaysOnBottom: true,
+    fullscreen: true,
+    autoHideMenuBar: true,
     backgroundColor: '#0b0d10',
     webPreferences: {
       contextIsolation: true,
@@ -14,16 +26,23 @@ function createWindow() {
     }
   });
 
+  win.setAlwaysOnBottom(true, 'screen-saver');
   win.loadFile(path.join(__dirname, 'index.html'));
+
+  globalShortcut.register('CommandOrControl+Shift+Q', () => app.quit());
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  if (process.platform === 'win32') {
+    const loginSettings = { openAtLogin: true };
+    if (!app.isPackaged) loginSettings.args = [app.getAppPath()];
+    app.setLoginItemSettings(loginSettings);
+  }
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+  createWindow();
 });
+
+app.on('will-quit', () => globalShortcut.unregisterAll());
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
